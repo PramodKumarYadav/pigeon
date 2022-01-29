40ab435b86c4bb5e39ab59a143977c7c7530e841
Pramod Kumar Yadav <pramodyadav027@gmail.com>
2022-01-30 00:10:17 +0100

    add edge options

--- src/main/java/factories/CapabilitiesFactory.java
+++ src/main/java/factories/CapabilitiesFactory.java
@@ -26,6 +26,7 @@ import config.EnvFactory;
 import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
@@ -54,6 +55,8 @@ public class CapabilitiesFactory {
                 return getFirefoxOptions();
             case OPERA:
                 return getOperaOptions();
+            case EDGE:
+                return getEdgeOptions();
             default:
                 throw new IllegalStateException(String.format("%s is not a valid browser choice. Pick your browser from %s.", browser, java.util.Arrays.asList(browser.values())));
         }
@@ -91,5 +94,9 @@ public class CapabilitiesFactory {
 
     public static OperaOptions getOperaOptions() {
         return new OperaOptions();
+    }
+
+    public static EdgeOptions getEdgeOptions() {
+        return new EdgeOptions();
     }
 }
