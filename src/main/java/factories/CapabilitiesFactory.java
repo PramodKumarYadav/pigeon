64433d4d96c560c5045f3c12162dbffe2d208bcd
Pramod Kumar Yadav <pramodyadav027@gmail.com>
2022-01-29 18:46:17 +0100

    updated

--- src/main/java/factories/CapabilitiesFactory.java
+++ src/main/java/factories/CapabilitiesFactory.java
@@ -1,22 +1,22 @@
 /**
  * Some references for debugging.
-    chromeOptions.addArguments("--verbose");
-    chromeOptions.addArguments("--whitelisted-ips=''");
-    chromeOptions.addArguments("--no-sandbox");
-    chromeOptions.addArguments("--disable-dev-shm-usage");
-    System.setProperty("webdriver.chrome.whitelistedIps", "");
-    //add this line for verbose logging
-    System.setProperty("webdriver.chrome.verboseLogging", "true");
-    chromeOptions.addArguments("--proxy-server='direct://'");
-    chromeOptions.addArguments("--proxy-bypass-list=*");
-    chromeOptions.setProxy(null);
-    https://www.selenium.dev/documentation/en/webdriver/page_loading_strategy/
-
-    * these lines enable debugging
-    System.setProperty("webdriver.chrome.logfile", "/usr/src/chromedrivergrid.log");
-    System.setProperty("webdriver.chrome.verboseLogging", "true");
-    WebDriverManager.globalConfig().setProxy("https://proxy.internal.organisation-name.org:8080");
-*/
+ * chromeOptions.addArguments("--verbose");
+ * chromeOptions.addArguments("--whitelisted-ips=''");
+ * chromeOptions.addArguments("--no-sandbox");
+ * chromeOptions.addArguments("--disable-dev-shm-usage");
+ * System.setProperty("webdriver.chrome.whitelistedIps", "");
+ * //add this line for verbose logging
+ * System.setProperty("webdriver.chrome.verboseLogging", "true");
+ * chromeOptions.addArguments("--proxy-server='direct://'");
+ * chromeOptions.addArguments("--proxy-bypass-list=*");
+ * chromeOptions.setProxy(null);
+ * https://www.selenium.dev/documentation/en/webdriver/page_loading_strategy/
+ * <p>
+ * these lines enable debugging
+ * System.setProperty("webdriver.chrome.logfile", "/usr/src/chromedrivergrid.log");
+ * System.setProperty("webdriver.chrome.verboseLogging", "true");
+ * WebDriverManager.globalConfig().setProxy("https://proxy.internal.organisation-name.org:8080");
+ */
 
 package factories;
 
@@ -41,10 +41,10 @@ import static java.lang.Boolean.parseBoolean;
 public class CapabilitiesFactory {
     private static Config config = EnvFactory.getInstance().getConfig();
 
-    static boolean headlessFlag = parseBoolean(config.getString("headless"));
-    static boolean acceptInsecureCertsFlag = parseBoolean(config.getString("acceptInsecureCertificates"));
-    static String seleniumLogLevel = config.getString("SELENIUM_LOG_LEVEL");
-    private final static String DOWNLOADS_DIR = config.getString("DOWNLOADS_DIR");
+    private static final boolean HEADLESS = parseBoolean(config.getString("HEADLESS"));
+    private static final boolean acceptInsecureCertsFlag = parseBoolean(config.getString("acceptInsecureCertificates"));
+    private static final String seleniumLogLevel = config.getString("SELENIUM_LOG_LEVEL");
+    private static final String DOWNLOADS_DIR = config.getString("DOWNLOADS_DIR");
 
     public static Capabilities getCapabilities(Browser browser) {
         switch (browser) {
@@ -62,7 +62,7 @@ public class CapabilitiesFactory {
     public static ChromeOptions getChromeOptions() {
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.setAcceptInsecureCerts(acceptInsecureCertsFlag);
-        chromeOptions.setHeadless(headlessFlag);
+        chromeOptions.setHeadless(HEADLESS);
 //        chromeOptions.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
         chromeOptions.addArguments("--no-sandbox"); // overcome limited resource problems and a must-have step to run tests in docker pipeline
         chromeOptions.addArguments("--window-size=1920,1080");
@@ -85,7 +85,7 @@ public class CapabilitiesFactory {
     public static FirefoxOptions getFirefoxOptions() {
         FirefoxOptions firefoxOptions = new FirefoxOptions();
         firefoxOptions.setAcceptInsecureCerts(acceptInsecureCertsFlag);
-        firefoxOptions.setHeadless(headlessFlag);
+        firefoxOptions.setHeadless(HEADLESS);
         return firefoxOptions;
     }
 
