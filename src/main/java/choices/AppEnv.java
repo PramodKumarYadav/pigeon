5efc01e0be22b3382aea1a5e53a93fbd11687d69
Pramod Kumar Yadav <pramodyadav027@gmail.com>
2022-01-29 18:23:47 +0100

    updated app env file.

--- src/main/java/choices/AppEnv.java
+++ src/main/java/choices/AppEnv.java
@@ -3,30 +3,29 @@ package choices;
 import java.util.HashMap;
 import java.util.Map;
 
-public enum Host {
-    LOCAL("local"),
-    DEVELOP("develop"),
-    STAGING("staging");
+public enum AppEnv {
+    DEVELOP("app.env.develop"),
+    STAGING("app.env.staging");
 
     public final String label;
 
-    Host(String label) {
+    AppEnv(String label) {
         this.label = label;
     }
 
-    private static final Map<String, Host> BY_LABEL = new HashMap<>();
+    private static final Map<String, AppEnv> BY_LABEL = new HashMap<>();
 
     static {
-        for (Host host : values()) {
-            BY_LABEL.put(host.label, host);
+        for (AppEnv appEnv : values()) {
+            BY_LABEL.put(appEnv.label, appEnv);
         }
     }
 
     // To get enum name from a label (choice specified in application.conf)
-    public static Host parse(String label) {
+    public static AppEnv parse(String label) {
         if (BY_LABEL.get(label) == null) {
-            throw new IllegalStateException(String.format("%s is not a valid host env choice. Pick your host env from %s." +
-                    "Check the value of 'HOST' property in choices.conf; Or in CI, if running from continuous integration.", label, BY_LABEL.keySet()));
+            throw new IllegalStateException(String.format("%s is not a valid app env choice. Pick your app env from %s." +
+                    "Check the value of 'APP_ENV' property in choices.conf; Or in CI, if running from continuous integration.", label, BY_LABEL.keySet()));
         } else {
             return BY_LABEL.get(label);
         }
