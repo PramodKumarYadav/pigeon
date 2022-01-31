cdec614747ebc473b9a6ed72c476ef2f1cd3ed74
Pramod Kumar Yadav <pramodyadav027@gmail.com>
2022-01-31 20:34:14 +0100

    added home page, tests and config.

---
+++ src/main/java/fedex/pages/HomePage.java
@@ -0,0 +1,52 @@
+package fedex.pages;
+
+import actions.PageActions;
+import com.typesafe.config.Config;
+import config.EnvFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class HomePage {
+    private PageActions pageActions;
+
+    private static Config config = EnvFactory.getInstance().getConfig();
+    private static final String HOME_PAGE_URL = config.getString("HOME_PAGE_URL");
+
+    public HomePage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+        pageActions = new PageActions(driver);
+    }
+
+    @FindBy(css = "button[class='fxg-cookie-consent__accept fxg-button fxg-button--orange js-fxg-cookie-save is-save-all']")
+    private WebElement acceptAllCookies;
+
+    @FindBy(css = "a[data-analytics=\"foc|Find Out More\"]")
+    private WebElement deliveryChoicesFindOutMoreButton;
+
+    public HomePage navigateToHomePageURL() {
+        pageActions.getPageWithRetry(HOME_PAGE_URL, By.cssSelector("button[id='btnSingleTrack']"));
+        return this;
+    }
+
+    public String getHomePageTitle() {
+        return pageActions.getPageTitle();
+    }
+
+    public HomePage acceptAllCookies() {
+        pageActions.scrollIntoViewAndClick(acceptAllCookies);
+        return this;
+    }
+
+    /** sugar syntax */
+    public HomePage and() {
+        return this;
+    }
+
+    public HomePage clickDeliveryChoicesFindOutMoreButton() {
+        pageActions.clickViaJavaScript(deliveryChoicesFindOutMoreButton);
+        return this;
+    }
+}
---
+++ src/test/java/fedex/pages/TestHomePage.java
@@ -0,0 +1,47 @@
+package fedex.pages;
+
+import actions.PageActions;
+import com.typesafe.config.Config;
+import config.EnvFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import testextensions.TestExecutionLifecycle;
+import testextensions.TestSetup;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Tag("regression")
+@ExtendWith(TestExecutionLifecycle.class)
+public class TestHomePage extends TestSetup {
+    private HomePage homePage;
+    private PageActions pageActions;
+    private static Config config = EnvFactory.getInstance().getConfig();
+    private static final String CAMPAIGN_PAGE_URL = config.getString("CAMPAIGN_PAGE_URL");
+    private static final String HOME_PAGE_TITLE = config.getString("HOME_PAGE_TITLE");
+
+    @BeforeEach
+    void initialize() {
+        homePage = new HomePage(driver).
+                navigateToHomePageURL().
+                and().
+                acceptAllCookies();
+
+        pageActions = new PageActions(driver);
+    }
+
+    @Tag("smokeTest")
+    @Test
+    void assertThatHomePageTitleIsCorrect() {
+        assertEquals(HOME_PAGE_TITLE, homePage.getHomePageTitle());
+    }
+
+    @Test
+    @DisplayName("Testing a button from main section on Home Page")
+    void assertThatClickingOnDeliveryChoicesFindOutMoreButtonRedirectsToTheCorrectPage() {
+        homePage.clickDeliveryChoicesFindOutMoreButton();
+        assertEquals(CAMPAIGN_PAGE_URL, pageActions.getCurrentPageURL());
+    }
+}
