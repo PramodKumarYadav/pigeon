6a2a2f83cf394a9ea1149799127f215cfb518ed2
Pramod Kumar Yadav <pramodyadav027@gmail.com>
2022-02-02 23:46:45 +0100

    add different test runners

--- src/test/java/cucumber/TestRunnerAlert.java
+++ src/test/java/cucumber/TestRunnerAlert.java
@@ -6,9 +6,9 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/resources/features/pages",
+        features = "src/test/resources/features/commonSections",
         glue = {"cucumber/stepDefinitions"},
         plugin = {"pretty"}
 )
-public class TestRunnerHomePage {
+public class TestRunnerAlert {
 }
