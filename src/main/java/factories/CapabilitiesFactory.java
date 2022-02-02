970d10247ddd5de98a9c85d9ffbe2f98ebdb665f
Pramod Kumar Yadav <pramodyadav027@gmail.com>
2022-02-02 23:47:08 +0100

    updated capabilites

--- src/main/java/factories/CapabilitiesFactory.java
+++ src/main/java/factories/CapabilitiesFactory.java
@@ -69,11 +69,11 @@ public class CapabilitiesFactory {
         chromeOptions.setHeadless(HEADLESS);
         chromeOptions.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems and a must-have step to run tests in docker pipeline or docker selenium grid
         chromeOptions.addArguments("--no-sandbox"); // overcome limited resource problems and a should-have step to run tests in docker pipeline or docker selenium grid
-//        chromeOptions.addArguments("--window-size=1920,1080");
-        chromeOptions.addArguments("--window-size=1200,600");
+        chromeOptions.addArguments("--window-size=1920,1080");
         chromeOptions.addArguments("start-maximized");
         chromeOptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
         chromeOptions.addArguments("--enable-javascript");
+        chromeOptions.addArguments("--disable-notifications");
 
         Map<String, Object> prefs = new Hashtable<String, Object>();
         prefs.put("plugins.always_open_pdf_externally", true);
