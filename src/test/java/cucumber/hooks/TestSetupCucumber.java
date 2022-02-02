c92fc990b557a9067408bbd23c2966b5c5f17213
Pramod Kumar Yadav <pramodyadav027@gmail.com>
2022-02-02 23:47:48 +0100

    keep it just to see if we can make use of if later

---
+++ src/test/java/cucumber/hooks/TestSetupCucumber.java
@@ -0,0 +1,29 @@
+package cucumber.hooks;
+
+import factories.DriverFactory;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import lombok.extern.slf4j.Slf4j;
+import org.openqa.selenium.WebDriver;
+import testextensions.PublishTestResults;
+
+@Slf4j
+public class TestSetupCucumber {
+    public WebDriver driver;
+
+    @Before
+    public void setUp() {
+        this.driver = DriverFactory.getDriver();
+        DriverFactory.setDriverTimeouts(driver);
+    }
+
+    @After
+    public void postProcessing() {
+        driver.close();
+        driver.quit();
+        log.info("tear down complete");
+
+        PublishTestResults.toElastic();
+        log.info("Published results to Elastic");
+    }
+}
