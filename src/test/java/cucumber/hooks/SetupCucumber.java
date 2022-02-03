428193d626127d4b990f7247bfad49ad0fa30825
Pramod Kumar Yadav <pramodyadav027@gmail.com>
2022-02-03 17:50:23 +0100

    renamed class

--- src/test/java/cucumber/hooks/SetupCucumber.java
+++ src/test/java/cucumber/hooks/SetupCucumber.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebDriver;
 import testextensions.PublishTestResults;
 
 @Slf4j
-public class TestSetupCucumber {
+public class SetupCucumber {
     public WebDriver driver;
 
     @Before
