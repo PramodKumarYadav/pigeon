ca18c039d44b70cc715db4b50c861bf800a076c3
Pramod Kumar Yadav <pramodyadav027@gmail.com>
2022-02-11 14:17:48 +0100

    refactored body to its own class

--- src/main/java/testextensions/PublishTestResults.java
+++ src/main/java/testextensions/PublishTestResults.java
@@ -1,6 +1,5 @@
 package testextensions;
 
-import choices.Host;
 import com.typesafe.config.Config;
 import config.EnvFactory;
 import io.restassured.RestAssured;
@@ -8,44 +7,29 @@ import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import lombok.extern.slf4j.Slf4j;
 
-import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
-
+@Slf4j
 public class PublishTestResults {
-    private static final String TEST_RUN = String.format("RUN-%s", System.currentTimeMillis());
-
     private static Config config = EnvFactory.getInstance().getConfig();
     private static final String MONITORING_INFRA_READY_ON_ELASTIC_KIBANA = config.getString("MONITORING_INFRA_READY_ON_ELASTIC_KIBANA");
 
     public static void toElastic() {
-        if(MONITORING_INFRA_READY_ON_ELASTIC_KIBANA.equalsIgnoreCase("true")){
-            publishResultsOnElastic();
+        if (MONITORING_INFRA_READY_ON_ELASTIC_KIBANA.equalsIgnoreCase("true")) {
+            TestRunBody testRunBody = new TestRunBody().setDefault();
+            log.info("testRunBody: {}", testRunBody);
+            publishResultsOnElastic(testRunBody);
         }
     }
 
-    private static Response publishResultsOnElastic() {
+    private static Response publishResultsOnElastic(TestRunBody testRunBody) {
         return RestAssured
                 .given()
                 .spec(getElasticBasicSpecification())
-                .body(getTestRunMap())
+                .body(testRunBody)
                 .when()
                 .post("/testruns/run")
                 .then().log().ifError().extract().response();
-    }
-
-    private static Map<String, String> getTestRunMap() {
-        Map<String, String> testResult = new HashMap<>();
-        testResult.put("project", "pigeon");
-        testResult.put("testRun", TEST_RUN);
-        testResult.put("testClass", TestExecutionLifecycle.getTestClassName());
-        testResult.put("testName", TestExecutionLifecycle.getTestName());
-        testResult.put("status", TestExecutionLifecycle.getTestResult());
-        testResult.put("reason", TestExecutionLifecycle.getReason());
-        testResult.put("executionTime", LocalDateTime.now().toString());
-        testResult.put("triggeredBy", System.getProperty("user.name")); //platform independent
-        return testResult;
     }
 
     private static RequestSpecification getElasticBasicSpecification() {
